5d61dc64825eae3d6836fcec5bdaa902fd5543ff
Bekhruz Rakhmonov <bekhruzrakhmonov1@gmail.com>
2023-10-21 01:37:32 +0200

    task 5 added

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,10 @@
-import express, { NextFunction, Request, Response, Router } from 'express'
-import * as userService from '../services/users'
-import RequestWithUser from '../interfaces/requestWithUser'
-import { deleteUserValidator, editUsersValidator } from '../validators/users'
-import { validationResult } from 'express-validator'
+import express, { NextFunction, Request, Response, Router } from 'express';
+import axios from 'axios';
+import * as userService from '../services/users';
+import RequestWithUser from '../interfaces/requestWithUser';
+import { deleteUserValidator, editUsersValidator, isValidRegion } from '../validators/users';
+import { validationResult } from 'express-validator';
+import { generateErrors } from '../services/users';
 
 const router: Router = express.Router()
 
@@ -77,4 +79,75 @@ router.post(
     },
 )
 
-export default router
+const RANDOMUSER_API_URL: string = 'https://randomuser.me/api/'
+
+interface User {
+    login: {
+        uuid: string
+    }
+    name: {
+        first: string
+        last: string
+    }
+    location: {
+        street: {
+            name: string
+        }
+        city: string
+        country: string
+    }
+    phone: string
+}
+
+router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+    const { region, errors, seed, page } = req.query as {
+        region: string
+        errors: string
+        seed: string
+        page: string
+    }
+
+    try {
+        res.header('Cache-Control', 'no-store, max-age=0')
+        if (isValidRegion(region)) {
+            return res
+                .status(400)
+                .json({ error: 'Invalid or unsupported region' })
+        }
+
+        const response = await axios.get(
+            `${RANDOMUSER_API_URL}?results=20&nat=${region}&seed=${seed}`,
+        )
+        const usersData: Array<{
+            randomIdentifier: string
+            name: string
+            address: string
+            phone: string
+        }> = (response.data.results as User[]).map((user: User) => ({
+            randomIdentifier: user.login.uuid,
+            name: `${user.name.first} ${user.name.last}`,
+            address: `${user.location.street.name}, ${user.location.city}, ${user.location.country}`,
+            phone: user.phone,
+        }))
+
+        const users = []
+        for (let userData of usersData) {
+            const [modifiedName, modifiedAddress] = await generateErrors(
+                userData.name,
+                userData.address,
+                parseInt(errors),
+                seed,
+            )
+            users.push({
+                ...userData,
+                name: modifiedName,
+                address: modifiedAddress,
+            })
+        }
+        res.json(users)
+    } catch (error) {
+        next(error)
+    }
+});
+
+export default router;
--- src/validators/users.ts
+++ src/validators/users.ts
@@ -1,4 +1,5 @@
-import { body, param } from 'express-validator'
+import { body } from 'express-validator'
+import { SupportedNats } from '../enums/nat'
 
 export const editUsersValidator = [
     body('userIds', 'Invalid does not Empty').not().isEmpty(),
@@ -9,3 +10,7 @@ export const deleteUserValidator = [
     body('userIds', 'Invalid does not Empty').not().isEmpty(),
     body('userIds', 'Invalid does not Array').isArray({ min: 1 }),
 ]
+
+export function isValidRegion(region: string): region is SupportedNats {
+    return Object.values(SupportedNats).includes(region as SupportedNats)
+}
